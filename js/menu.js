11d04376c5448be25433490f746cec06115ec00c
Jensen Bouzi <jensen.bouzi@gmail.com>
2015-01-08 01:20:03 -0500

    add javascript for scrolling to top

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,33 @@
-$(document).ready(function() {  
-  $('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-          || location.hostname == this.hostname) {
+// $(document).ready(function() {  
+//   $('a[href*=#]:not([href=#])').click(function() {
+//       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+//           || location.hostname == this.hostname) {
 
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-             if (target.length) {
-               $('html,body').animate({
-                   scrollTop: target.offset().top
-              }, 1000);
-              return false;
-          }
-      }
-  });
+//           var target = $(this.hash);
+//           target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//              if (target.length) {
+//                $('html,body').animate({
+//                    scrollTop: target.offset().top
+//               }, 1000);
+//               return false;
+//           }
+//       }
+//   });
+// })
+$(document).ready(function() {
+  $('.scroll-top-wrapper').on('click', scrollToTop);
 })
+function scrollToTop() {
+  verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+  element = $('body');
+  offset = element.offset();
+  offsetTop = offset.top;
+  $('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+}
+$(document).on( 'scroll', function(){
+  if ($(window).scrollTop() > 50) {
+    $('.scroll-top-wrapper').addClass('show');
+  } else {
+    $('.scroll-top-wrapper').removeClass('show');
+  }
+});
