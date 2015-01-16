6dd628454cbcf30e22cbe254840517de4e4b802b
Jensen Bouzi <jensen.bouzi@gmail.com>
2015-01-16 00:46:07 -0500

    nav links jump to each section

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,9 @@
 // $(document).ready(function() {  
-//   $('a[href*=#]:not([href=#])').click(function() {
-//       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-//           || location.hostname == this.hostname) {
-
-//           var target = $(this.hash);
-//           target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-//              if (target.length) {
-//                $('html,body').animate({
-//                    scrollTop: target.offset().top
-//               }, 1000);
-//               return false;
-//           }
-//       }
-//   });
+//   
 // })
 $(document).ready(function() {
   $('.scroll-top-wrapper').on('click', scrollToTop);
+
 })
 function scrollToTop() {
   verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
