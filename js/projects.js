332f7216c69562624e05849b9593c74e0d5436b6
Jensen Bouzi <jensen.bouzi@gmail.com>
2015-02-21 13:52:23 -0500

    images fade in after loading

--- js/projects.js
+++ js/projects.js
@@ -2,4 +2,13 @@ $(document).ready(function() {
 	$('#nav-button').click(function() {
 		$(".slide-menu").addClass("is-visible");
 	})
+	$('.fadeable').each (function(i, img) {
+		$(img).load(function(e) {
+			$(this).fadeIn(10000);
+		})
+	});
+
+	$('.fadeable_last').load(function(e) {
+		$(this).fadeIn(14000);
+	})
 })
