445359fefc4a53427c322811737b2011ff40c6ad
Jensen Bouzi <jensen.bouzi@gmail.com>
2015-04-08 12:55:37 -0400

    add thank you message after submitting email form

--- js/projects.js
+++ js/projects.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
 	$('#nav-button').click(function() {
 		$(".slide-menu").toggleClass("is-visible");
 	})
-})
+	$('#button-blue').click(function() {
+		$('#form-div').hide();
+		$('.thank-you').show();
+	})
+});
