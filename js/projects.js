a47d423fc82e3fe66ac0c01f7f344de8694b59a9
Jensen Bouzi <jensen.bouzi@gmail.com>
2015-06-12 18:07:49 -0400

    configure wow.js

--- js/projects.js
+++ js/projects.js
@@ -1,4 +1,16 @@
+  wow = new WOW(
+    {
+      boxClass:     'wow',      // default
+      animateClass: 'animated', // default
+      offset:       0,          // default
+      mobile:       true,       // default
+      live:         true        // default
+    }
+  )
 $(document).ready(function() {
+	
+	wow.init();
+	
 	$('#nav-button').click(function() {
 		$(".slide-menu").toggleClass("is-visible");
 	})
